69ddf3f8f62421490932776b3de196ee8e4d1a1d
Kaltag1925 <pdavisbray@gmail.com>
2021-11-29 08:50:56 -0600

    updating

--- app/map.js
+++ app/map.js
@@ -1,35 +1,45 @@
 
-var height = 600;
-var width = 600;
+
+var height = 5968;
+var width = 7152;
 
 var k = height / width;
 
-dataRaw = await d3.csv("data.csv")
+// dataRaw = await d3.csv("data.csv")
 
 
-var randomLocation = () => ({x: Math.round(Math.random() * (4 - -4) + -4), y: Math.round(Math.random() * (4 - -4) + -4)})
-cities = dataRaw.map(a => a.City).reduce((cs, curCity) => cs.add(curCity), new Set())
-cityLocations = Array.from(cities).map(c => ({name: c, location: randomLocation()}))
+// var randomLocation = () => ({x: Math.round(Math.random() * (4 - -4) + -4), y: Math.round(Math.random() * (4 - -4) + -4)})
+// cities = dataRaw.map(a => a.City).reduce((cs, curCity) => cs.add(curCity), new Set())
+// cityLocations = Array.from(cities).map(c => ({name: c, location: randomLocation()}))
 
-console.log(cities)
-data = cityLocations.map(c => ({city: c, amount: 0}))
-dataRaw.forEach(s => {
-	const city = data.find(c => c.city.name == s.City)
-	city.amount = city.amount + 1
-})
+// console.log(cities)
+// data = cityLocations.map(c => ({city: c, amount: 0}))
+// dataRaw.forEach(s => {
+// 	const city = data.find(c => c.city.name == s.City)
+// 	city.amount = city.amount + 1
+// })
 
-loadLayout()
-var z = d3.scaleOrdinal()
-	.domain(data.map(d => d[2]))
-	.range(d3.schemeCategory10)
+// var z = d3.scaleOrdinal()
+// 	.domain(data.map(d => d[2]))
+// 	.range(d3.schemeCategory10)
 
-var y = d3.scaleLinear()
-	.domain([-4.5 * k, 4.5 * k])
-	.range([height, 0])
-	
-var x = d3.scaleLinear()
-	.domain([-4.5, 4.5])
-	.range([0, width])
+var x,y
+function loadMap() {
+	loadRanges()
+	loadVisuals()
+	console.log("ran")
+}
+
+function loadRanges() {
+
+	y = d3.scaleLinear()
+		.domain([0, height])
+		.range([0, height])
+		
+	x = d3.scaleLinear()
+		.domain([0, width])
+		.range([0, width])
+}
 
 function grid(g, x, y){
 	g.attr("stroke", "currentColor")
@@ -74,47 +84,19 @@ const zoom = d3.zoom()
 const svg = d3.select("#map").append("svg")
 	.attr("viewBox", [0, 0, width, height]);
 
+const image = svg.append("svg:image")
+
 const gGrid = svg.append("g");
 
 const gDot = svg.append("g")
 	.attr("fill", "red")
 	.attr("stroke-linecap", "round");
 
-gDot.append("g").append("image")
-.attr("opacity", .2)
-.attr("width", width)
-.attr("height", height)
-
-gDot.selectAll("path")
-.data(data)
-.join("circle")
-	.attr("opacity", .5)
-	.attr("cx", d => x(d.city.location.x))
-	.attr("cy", d => y(d.city.location.y))
-	.attr("stroke", "grey")
-	.attr("stroke-width", 1)
-	.attr("data", d => (d))
-	.attr("r", d => d.amount * 2)
-	.attr("text", d => d.city.name)
-	.attr("id", d => d.city.name)
-	
 const pointsLabels = gDot.append("g")
-.selectAll("pointLabelText")
-.data(data)
-.join("text")
-	.attr("opacity", 1)
-	.attr("fill", "black")
-	.attr("x", d => x(d.city.location.x))
-	.attr("y", d => y(d.city.location.y))
-	//.attr("transform", d => `rotate(-45,${x(d.location.x)},${y(d.location.y)})`)
-	.attr("text-anchor", "middle")
-	.text(d => d.city.name + ": " + d.amount)
 
 const gx = svg.append("g");
 
 const gy = svg.append("g");
-
-svg.call(zoom).call(zoom.transform, d3.zoomIdentity);
 
 function zoomed({transform}) {
 	const zx = transform.rescaleX(x).interpolate(d3.interpolateRound);
@@ -123,13 +105,55 @@ function zoomed({transform}) {
 	gx.call(xAxis, zx);
 	gy.call(yAxis, zy);
 	gGrid.call(grid, zx, zy);
+	console.log(transform)
+	image.attr("transform", `translate(${transform.x} ${transform.y})scale(${transform.k})`)
 }
 
-console.log(svg)
-Object.assign(svg.node(), {
-reset() {
-	svg.transition()
-		.duration(750)
-		.call(zoom.transform, d3.zoomIdentity);
+function loadVisuals() {
+
+	image.attr('x', x(0))
+	.attr('y', y(0))
+	.attr("xlink:href", "map.png")
+
+	gDot.append("g").append("image")
+	.attr("opacity", .2)
+	.attr("width", width)
+	.attr("height", height)
+
+	gDot.selectAll("path")
+	.data(Array.from(sourceData.cities.values()))
+	.join("circle")
+		.attr("opacity", .5)
+		.attr("cx", d => x(d.x))
+		.attr("cy", d => y(d.y))
+		.attr("stroke", "grey")
+		.attr("stroke-width", 1)
+		.attr("data", d => (d))
+		.attr("r", d => 10)//d.amount * 2)
+		.attr("text", d => d.name)
+		.attr("id", d => d.name)
+		
+	
+	pointsLabels.selectAll("pointLabelText")
+	.data(Array.from(sourceData.cities.values()))
+	.join("text")
+		.attr("opacity", 1)
+		.attr("fill", "black")
+		.attr("x", d => x(d.x))
+		.attr("y", d => y(d.y) - 20)
+		//.attr("transform", d => `rotate(-45,${x(d.location.x)},${y(d.location.y)})`)
+		.attr("text-anchor", "middle")
+		.text(d => d.name)// + ": " + d.amount)
+
+
+	svg.call(zoom).call(zoom.transform, d3.zoomIdentity);
+
+	console.log(svg)
+	Object.assign(svg.node(), {
+	reset() {
+		svg.transition()
+			.duration(750)
+			.call(zoom.transform, d3.zoomIdentity);
+	}
+	});
 }
-});
---
+++ app/model.js
@@ -0,0 +1,51 @@
+function loadData(){
+    // Student :: ([ID], [Name, CityID]
+    sourceData = {students: new Map(), cities: new Map()}
+    d3.csv("data.csv").then(d => loadStudents(d)).then(
+        d3.csv("cityLocations.csv").then(d => loadCities(d)).then(d =>
+            loadLayout()).then(d =>
+                loadMap()
+            ))
+}
+
+function buildStudent(student) {
+    return {first: student.First, 
+        middle: student.Middle, 
+        last: student.Last, 
+        cityID: student.City + student.State}
+}
+
+function studentID(student) {
+    return student.First + student.Middle + student.Last + student.City + student.State
+}
+
+function loadStudents(data) {
+    var studs = new Map();
+    data.forEach(s => studs.set(studentID(s), buildStudent(s)))
+    sourceData.students = studs   
+}
+
+function buildCity(city) {
+    var x, y
+    if (city.X) {
+        x = city.X
+        y = city.Y
+    } else {
+        x = 0//Math.round(Math.random() * 3000)
+        y = 0//Math.round(Math.random() * 3000)
+    }
+
+    return {name: city.City, state: city.State, x: x, y: y}
+}
+
+function cityID(city) {
+    return city.City + city.State
+}
+
+function loadCities(data) {
+    var cits = new Map();
+    console.log(data)
+    data.forEach(c => cits.set(cityID(c), buildCity(c)))
+    console.log(cits)
+    sourceData.cities = cits
+}
